58ee2487003f6731284ad81f5192b2027b3c0458
parrajulio33 <parrajulio33@gmail.com>
2018-11-22 16:49:13 -0700

    Create-a-Variable-Let

--- Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable.js
@@ -2,3 +2,10 @@ var favoriteFood = 'pizza';
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+// CREATE VARIABLE LET
+let changeMe = true;
+
+changeMe = false;
+
+console.log(changeMe);
