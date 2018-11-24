bf57778cc62ca6513035b2bb8f0c4f57fae03bc3
parrajulio33 <parrajulio33@gmail.com>
2018-11-24 10:45:35 -0700

    Accessing-Elements

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,6 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = 'Fortune favors the brave.';
+
+console.log(listItem);
+console.log(famousSayings[3]);
