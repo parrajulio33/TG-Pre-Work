0d2f09ec9f5927d60e033c3dc60f6d3c06bd54d0
parrajulio33 <parrajulio33@gmail.com>
2018-11-24 16:53:11 -0700

    Repeating-Tasks-Manually

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,6 @@
+// Write your code below
+const vacationSpots = ['hong kong', 'hawai', 'singapure'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
