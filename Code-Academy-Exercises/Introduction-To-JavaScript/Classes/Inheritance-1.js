a1e151e70c60927f3d0a9e8720cfa1fa8b9d0bae
parrajulio33 <parrajulio33@gmail.com>
2018-11-25 02:25:49 -0700

    Inheritance

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-1.js
@@ -0,0 +1,18 @@
+class Animal {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
